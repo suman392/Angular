4615d46eca72b4dc80fca287b79b637a94de395e
suman392 <suman2015rjit@gmail.com>
2021-12-09 16:20:51 +0530

    hostbinding and binding directives

--- ngIf-ngFor/src/app/better-highlight/better-highlight-directive.ts
+++ ngIf-ngFor/src/app/better-highlight/better-highlight-directive.ts
@@ -1,22 +1,33 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from "@angular/core";
 
 @Directive ({
     selector : '[appBetterhighlight]'
 })
 
 export class BetterHighlightDirective implements OnInit {
+
+    @Input() defaultColor : string ='transparent';
+    @Input() highlightColor : string = 'red';
+
+    @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+
     constructor ( private moonRef : ElementRef , private renderer : Renderer2) {
     }
     ngOnInit(){
         // this.moonRef.nativeElement.style.backgroundColor = "blue";
+        this.backgroundColor = this.defaultColor ; // so that blue color will be there in the begnning.
     }
 
     @HostListener('mouseenter') mouseover(eventData :Event){
-        this.renderer.setStyle(this.moonRef.nativeElement, 'backgroundColor', 'blue');
+        // this.renderer.setStyle(this.moonRef.nativeElement, 'backgroundColor', 'blue'); //hostlistener
+        // this.backgroundColor = "orange"; //hostbinding
+        this.backgroundColor = this.highlightColor; //binding
     }
 
     @HostListener('mouseleave') mouseleave(eventData :Event){
-        this.renderer.setStyle(this.moonRef.nativeElement, 'backgroundColor', 'transparent');
+        // this.renderer.setStyle(this.moonRef.nativeElement, 'backgroundColor', 'transparent'); //hostlistener
+        // this.backgroundColor = "transparent";//hostbinding
+        this.backgroundColor = this.defaultColor ; //binding
     }
 
     
